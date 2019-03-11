f4d60ae6996e1a9903d821753fca01a9f8c525c0
yuemintang <shirleytang1996@gmail.com>
2019-03-11 16:33:17 -0400

    commit 4
    set up the basic design

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,17 +2,47 @@ import React, { Component } from 'react';
 
 import './App.css';
 
+import Container from 'react-bootstrap/Container';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-        
-          <p>
-           test
-          </p>
-          
-        </header>
+        <Container>
+          <header className="App-header">
+            <h1>React Based ToDo List</h1>
+          </header> 
+          <ListGroup>
+            <ListGroup.Item>
+              <Card className="bg-light border rounded">
+                <Card.Body className="text-left">
+                  <h5 >This is a title</h5>
+                  <p>This is the description</p>
+                </Card.Body>
+              </Card>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Card className="bg-light border rounded">
+                <Card.Body className="text-left">
+                  <h5 >This is another title</h5>
+                  <p>This is another description</p>
+                </Card.Body>
+              </Card>
+            </ListGroup.Item>
+          </ListGroup>
+          <h2>Add A ToDo</h2>
+          <Form className="text-left">
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Title</Form.Label>
+              <Form.Control type="text" placeholder="Enter Title" />
+              <Form.Label>Description</Form.Label>
+              <Form.Control as="textarea" rows="3" placeholder="Enter Description" />
+            </Form.Group>
+          </Form>
+          </Container>
       </div>
     );
   }
